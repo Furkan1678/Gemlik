0838e31a20a24ff58611eca346e67b175bbda50c
Burak Öveç <arceliktv3737@gmail.com>
2025-04-09 12:19:11 +0300

    deneme

--- backend/IssueTrackerPro/IssueTrackerPro.API/obj/Debug/net8.0/IssueTrackerPro.API.AssemblyInfo.cs
+++ backend/IssueTrackerPro/IssueTrackerPro.API/obj/Debug/net8.0/IssueTrackerPro.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IssueTrackerPro.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81b2dc26d56ae63ecd3451548ff95ab58d96bc14")]
 [assembly: System.Reflection.AssemblyProductAttribute("IssueTrackerPro.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IssueTrackerPro.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
